d7ebd5e529a4a303e98920c61c5daa1c0348f749
fnaf.cracks@gmail.com <fnaf.cracks@gmail.com>
2025-05-22 19:01:01 +0900

    Not working

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -152,7 +152,7 @@ process_exit (void)
 
   /* Destroy the current process's page directory and switch back
      to the kernel-only page directory. */
-  printf("s: exit(%d)\n", curr->name, curr->exit_code);
+  printf("s: exit(%d)\n", cur->name, cur->exit_code);
   pd = cur->pagedir;
   if (pd != NULL) 
     {
